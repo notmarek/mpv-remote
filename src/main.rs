a05c6fd64ab2b5eb793f6e204554bb451169177c
Marek Veselý <veselym@spstrutnov.cz>
2021-07-21 18:43:05 +0200

    Added a pause function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,85 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use mpvipc::*;
+use serde::{
+    de::{self, Visitor},
+    Deserialize, Deserializer, Serialize, Serializer,
+};
 use std::{thread, time};
+
+pub enum PlayerState {
+    Playing,
+    Paused,
+    Error(String),
+}
+struct PlayerStateVisitor;
+
+impl<'de> Visitor<'de> for PlayerStateVisitor {
+    type Value = PlayerState;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("string \"playing\" or \"paused\"")
+    }
+
+    fn visit_string<E>(self, value: String) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        if value == "playing" {
+            Ok(PlayerState::Playing)
+        } else if value == "paused" {
+            Ok(PlayerState::Paused)
+        } else if value == "error" {
+            Ok(PlayerState::Error("error".to_string()))
+        } else {
+            Err(de::Error::custom("Unexpected string"))
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for PlayerState {
+    fn deserialize<D>(deserializer: D) -> Result<PlayerState, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_string(PlayerStateVisitor)
+    }
+}
+impl Serialize for PlayerState {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            PlayerState::Playing => "playing",
+            PlayerState::Paused => "paused",
+            PlayerState::Error(e) => e,
+        }
+        .serialize(serializer)
+    }
+}
+pub enum Status {
+    Success,
+    Error,
+}
+
+impl Serialize for Status {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            Status::Success => "success",
+            Status::Error => "error",
+        }
+        .serialize(serializer)
+    }
+}
+
+#[derive(Serialize)]
+pub struct Response<T: Serialize> {
+    status: Status,
+    data: T,
+}
 
 async fn play_test() -> impl Responder {
     let mpv = get_mpv();
@@ -10,6 +89,29 @@ async fn play_test() -> impl Responder {
     "Video playback should've started."
 }
 
+async fn pause() -> impl Responder {
+    let mpv = get_mpv();
+    let play_state = match mpv.get_property::<bool>("pause") {
+        Ok(s) => match s {
+            true => Err(PlayerState::Paused),
+            false => Ok(PlayerState::Playing),
+        },
+        Err(e) => Err(PlayerState::Error(format!("{}", e))),
+    };
+    match play_state {
+        Ok(s) => {
+            mpv.pause().unwrap();
+            HttpResponse::Ok().json(Response {
+                status: Status::Success,
+                data: "Paused playback.",
+            })
+        }
+        Err(e) => HttpResponse::Ok().json(Response {
+            status: Status::Error,
+            data: e,
+        }),
+    }
+}
 fn spawn_mpv() {
     match std::process::Command::new("mpv")
         .args(&[
@@ -37,8 +139,12 @@ fn get_mpv() -> Mpv {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || App::new().route("/", web::get().to(play_test)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .route("/", web::get().to(play_test))
+            .route("/test", web::get().to(pause))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
